097f33e4ab8415847a0b63c12247ec6b24e0833f
Szymon Mazur <smazur@mnfro.com>
2022-10-16 22:36:20 +0200

    init setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'moto-bid-platform';
+  username = 'Paweł';
+
+  changeNameToGuzik() {
+    console.log('tak');
+
+    this.username = 'Guzik';
+  }
 }
