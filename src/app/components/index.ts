72c83f85bd7fc00960267994e298e797918b00ca
Szymon Mazur <smazur@mnfro.com>
2022-10-22 15:18:02 +0200

    structure and login page ui improved

---
+++ src/app/components/index.ts
@@ -0,0 +1,6 @@
+export * from './login';
+export * from './main-page';
+export * from './register';
+export * from './start';
+export * from './user';
+export * from './app.component';
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  public modelForm!: UntypedFormGroup;
+  constructor(private router: Router, private formBuilder: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.createForm();
+  }
+
+  public login(): void {
+    // this.router.navigate(['/main']);
+    this.modelForm.reset();
+  }
+
+  private createForm(): void {
+    this.modelForm = this.formBuilder.group({
+      username: ['dupa', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+}
