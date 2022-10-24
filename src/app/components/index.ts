9f1e0896b22eb1da8795721c16a6bc4bb49cfd5f
Szymon Mazur <smazur@mnfro.com>
2022-10-24 20:10:02 +0200

    register page and navbar improved

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -4,3 +4,5 @@ export * from './register';
 export * from './start';
 export * from './user';
 export * from './app.component';
+export * from './menu';
+export * from './auctions-page';
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -10,32 +10,35 @@ import { CustomValidators } from 'src/app/utils';
 })
 export class RegisterComponent implements OnInit {
   public modelForm!: UntypedFormGroup;
+  public mismatchErrorMessage: string = '';
 
   constructor(private router: Router, private formBuilder: FormBuilder) {}
 
   public ngOnInit(): void {
     this.createForm();
     this.modelForm.addValidators(CustomValidators.MatchValidator('password', 'confirmPassword'));
-    console.log('dsad', this.modelForm);
   }
 
   public register(): void {
-    // this.router.navigate(['/main']);
+    this.router.navigate(['/main']);
     this.modelForm.reset();
   }
 
-  public get passwordMatchError(): string {
-    return this.modelForm.getError('mismatch') && this.modelForm.get('confirmPassword')?.touched
-      ? 'Password does not match'
-      : '';
+  public passwordMatchError(): void {
+    if (this.modelForm.getError('mismatch') && this.modelForm.get('confirmPassword')?.dirty) {
+      this.mismatchErrorMessage = 'Password does not match';
+      this.modelForm.controls['confirmPassword'].setErrors({ mismatch: true });
+    } else {
+      this.mismatchErrorMessage = '';
+    }
   }
 
   private createForm(): void {
     this.modelForm = this.formBuilder.group({
       username: ['', Validators.required],
       email: ['', Validators.required],
-      password: ['dupadupa', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['dupadupa1', [Validators.required, Validators.minLength(8)]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
     });
   }
 }
